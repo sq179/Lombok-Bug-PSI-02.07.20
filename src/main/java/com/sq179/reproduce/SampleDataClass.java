62e0e4c1c6eb588426c52a70bf866316f4a8a477
sq179 <dima-zagorodniy@ukr.net>
2020-07-02 10:28:52 +0300

    forget to comment (lol)

--- src/main/java/com/sq179/reproduce/SampleDataClass.java
+++ src/main/java/com/sq179/reproduce/SampleDataClass.java
@@ -3,7 +3,7 @@ package com.sq179.reproduce;
 import lombok.Data;
 
 @Data
-@SuppressWarnings("all") - Uncomment to get exception
+//@SuppressWarnings("all") - Uncomment to get exception
 public class SampleDataClass {
 
     private String text;
